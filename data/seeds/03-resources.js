24e5ad108664d6560a1a04de8fe1009915ae6261
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-08 09:18:43 -0800

    seeds resources

--- data/seeds/03-resources.js
+++ data/seeds/03-resources.js
@@ -1,13 +1,13 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  // Inserts seed entries
+  return knex('tasks').insert([
+    { name: 'hammer' },
+    { name: 'mover' },
+    { name: 'cardboard box' },
+    { name: 'electric drill' },
+    { name: 'screwdriver' },
+    { name: 'dolly' },
+    { name: 'computer' },
+    { name: 'vehicle' },
+  ]);
 };
