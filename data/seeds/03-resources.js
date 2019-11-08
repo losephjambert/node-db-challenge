47149586618a1d2408be4d7ae72f32d9cf63b4c7
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-08 09:22:20 -0800

    seeds project_resources

--- data/seeds/03-resources.js
+++ data/seeds/03-resources.js
@@ -1,13 +1,13 @@
 exports.seed = function(knex) {
   // Inserts seed entries
   return knex('resources').insert([
-    { name: 'hammer' },
-    { name: 'mover' },
-    { name: 'cardboard box' },
-    { name: 'electric drill' },
-    { name: 'screwdriver' },
-    { name: 'dolly' },
-    { name: 'computer' },
-    { name: 'vehicle' },
+    { name: 'hammer' }, // 1
+    { name: 'mover' }, // 2
+    { name: 'cardboard box' }, // 3
+    { name: 'electric drill' }, // 4
+    { name: 'screwdriver' }, // 5
+    { name: 'dolly' }, // 6
+    { name: 'computer' }, // 7
+    { name: 'vehicle' }, // 8
   ]);
 };
--- data/seeds/04-project_resources.js
+++ data/seeds/04-project_resources.js
@@ -1,13 +1,13 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  // Inserts seed entries
+  return knex('resources').insert([
+    { resource_id: 1, project_id: 1 },
+    { resource_id: 1, project_id: 2 },
+    { resource_id: 5, project_id: 2 },
+    { resource_id: 5, project_id: 3 },
+    { resource_id: 8, project_id: 2 },
+    { resource_id: 8, project_id: 3 },
+    { resource_id: 6, project_id: 2 },
+    { resource_id: 4, project_id: 3 },
+  ]);
 };
