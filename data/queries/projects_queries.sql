d25bb8841d06cbef16e8ab2026d8db157d0f2d5b
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-08 09:55:25 -0800

    sql queries to for a given project id, get all it's resources && for a given project id, get all it's tasks

---
+++ data/queries/projects_queries.sql
@@ -0,0 +1,16 @@
+select * from projects
+where projects.id = 2;
+
+-- for a given project id, get all it's resources
+select r.name
+from project_resources as pr
+join resources as r
+where pr.project_id = 2
+and r.id = pr.resource_id;
+
+-- for a given project id, get all it's tasks
+select t.description
+from projects as p
+join tasks as t
+on p.id = t.project_id
+where p.id = 2;
