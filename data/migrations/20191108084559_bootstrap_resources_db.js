07e9b4646185ba891b6b2d4126a9f7309bc28277
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-08 09:03:18 -0800

    fixes typo

--- data/migrations/20191108084559_bootstrap_resources_db.js
+++ data/migrations/20191108084559_bootstrap_resources_db.js
@@ -33,7 +33,7 @@ exports.up = function(knex) {
           .string('name', 255)
           .notNullable()
           .unique();
-        table.text('descrption', 500).notNullable();
+        table.text('description', 500).notNullable();
       })
 
       // project_resources table
